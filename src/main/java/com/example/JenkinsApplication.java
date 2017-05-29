d03ea59d4abd511eed5e079f38d9217fc2cc3fc3
liumaohua <9141787@qq.com>
2017-05-29 22:20:44 +0800

    'ttt'

--- src/main/java/com/example/JenkinsApplication.java
+++ src/main/java/com/example/JenkinsApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class JenkinsApplication {
 
+	s
 	public static void main(String[] args) {
 		SpringApplication.run(JenkinsApplication.class, args);
 	}
