8c8dcb9886cc28589451b07cecb847b9c38d43d3
liumaohua <9141787@qq.com>
2017-05-29 23:36:47 +0800

    add

--- src/main/java/com/example/JenkinsApplication.java
+++ src/main/java/com/example/JenkinsApplication.java
@@ -6,7 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class JenkinsApplication {
 
-	s
 	public static void main(String[] args) {
 		SpringApplication.run(JenkinsApplication.class, args);
 	}
